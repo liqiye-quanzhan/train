549e9d3720525f38c4bb003124e28c6383f8bf36
liqiye-quanzhan <2667907566@qq.com>
2021-07-02 11:48:24 +0800

    第三次上传企业站

--- qiyezhan/btn.js
+++ qiyezhan/btn.js
@@ -21,13 +21,10 @@ const imgs = document.querySelectorAll('.lazy')
                 imgs.forEach(image=>{//forEach() 方法用于调用数组的每个元素，并将元素传递给回调函数。
                     const imgTop = image.getBoundingClientRect().top //getBoundingClientRect()用于获得页面中某个元素的左，上，右和下分别相对浏览器视窗的位置。
                     if(imgTop<=window.innerHeight){//innerheight 返回窗口的文档显示区的高度。
-                        if(window.innerWidth<900){
-                            const zjsrc = "./aisi.webp"
-                            image.setAttribute('src',zjsrc)
-                        }else{
+                        
                             const src = image.getAttribute('data-src')//getAttribute() 方法通过名称获取属性的值。
                             image.setAttribute('src',src)
-                        }
+                        
                         
                     }
                 })
